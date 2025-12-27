587c0beea2b22e1d036f0c7221ce5eb97b0b0191
penglifang <penglifang@penglifang.net>
2025-12-27 18:42:32 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: 'Demo App',
+  },
+  tailwindcss: {},
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+  ],
+  npmClient: 'pnpm',
+});
+
--- create.js
+++
@@ -1,1 +0,0 @@
-var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/gh/libao123web/demome/FloatBall.js",document.body.appendChild(script);
